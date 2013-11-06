566d22e876527cce5aa0ef44a6c48e0e65299173
Robert Scott Dionne <robertsdionne@gmail.com>
2013-11-06 11:06:44 -0500

    Add the airplane script.

---
+++ Assets/scripts/AirplaneController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirplaneController : MonoBehaviour {
+	
+	public Camera mainCamera;
+
+	private Vector3 cameraOffset;
+	private float velocity;
+
+	// Use this for initialization
+	void Start() {
+		cameraOffset = mainCamera.transform.position - gameObject.transform.position;
+		velocity = 100.0f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		var yaw = 2.0f * Input.GetAxis("Horizontal");
+		var yawRotation = Quaternion.AngleAxis(yaw, Vector3.up);
+		gameObject.transform.rotation = yawRotation * gameObject.transform.rotation;
+		gameObject.transform.position += velocity * gameObject.transform.up * Time.deltaTime;
+	}
+	
+	void Update() {
+		var position = mainCamera.transform.position;
+		position.x = gameObject.transform.position.x + cameraOffset.x;
+		position.z = gameObject.transform.position.z + cameraOffset.z;
+		mainCamera.transform.position = position;
+	}
+}
