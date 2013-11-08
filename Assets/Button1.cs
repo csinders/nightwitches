17600397278ae15d8ae3a96567d1d653f2b38b47
csinders <csinders@gmail.com>
2013-11-07 21:17:06 -0500

    added start scene
    with buttons/images

---
+++ Assets/Button1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Button1 : MonoBehaviour {
+	
+	public Texture2D buttonOn;
+	public Texture2D buttonOff;
+			
+	public void OnMouseEnter(){
+		guiTexture.texture = buttonOn; 		
+	}
+	
+	public void OnMouseExit(){
+		guiTexture.texture = buttonOff;
+			
+	}
+	
+	public void OnMouseDown(){
+		Application.LoadLevel("MainScene");
+			
+	}
+		
+
+	
+}
