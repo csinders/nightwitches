8b1a84b04bc0c319aad11cdde9f34017a630916d
csinders <csinders@gmail.com>
2013-11-23 13:09:54 -0500

    remove 2-d tool kit
    see above

--- Assets/scripts/AirplaneController.cs
+++ Assets/scripts/AirplaneController.cs
@@ -3,29 +3,58 @@ using System.Collections;
 
 public class AirplaneController : MonoBehaviour {
 	
-	public Camera mainCamera;
+	public float accelerationRate; //push up on keyboard buttons, how fast plane increase or decrease speed
+	public float glideRate;  //natural decrease that is constantly slowing down 
+	public float heightFromSpeed;  //ratio between speed and height, speed controls airplane 
+	public Camera mainCamera; 
 
-	private Vector3 cameraOffset;
-	private float velocity;
+	private float speed; //not for the editor to have access to, calculated at start of game 
+	private float targetCameraAltitude;
+
+
 
 	// Use this for initialization
 	void Start() {
-		cameraOffset = mainCamera.transform.position - gameObject.transform.position;
-		velocity = 100.0f;
+		speed = 0.0f;  //this is at zero b/c of private float speed 
+		targetCameraAltitude = mainCamera.transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate() {
-		var yaw = 2.0f * Input.GetAxis("Horizontal");
-		var yawRotation = Quaternion.AngleAxis(yaw, Vector3.up);
-		gameObject.transform.rotation = yawRotation * gameObject.transform.rotation;
-		gameObject.transform.position += velocity * gameObject.transform.up * Time.deltaTime;
+		//get input from player into two variables 
+		var yaw = 2.0f * Input.GetAxis("Horizontal"); //gives input for LR arrows 
+		var acceleration = accelerationRate * Input.GetAxis("Vertical"); //gives input for Up Down arrows 
+
+
+		//update airplanes position and rotatoin 
+		gameObject.transform.rotation = CalculateNewRotation(gameObject.transform.rotation,yaw); //comma instead of dot b/c im passing two arguments 
+		gameObject.transform.position = CalculateNewPosition(gameObject.transform.position);
+
+		//update camera position
+		mainCamera.transform.position = CalculateNewCameraPosition(gameObject.transform.position);
+
+		//update the airplane's speed
+		speed = CalculateNewSpeed(acceleration);
 	}
 	
-	void Update() {
-		var position = mainCamera.transform.position;
-		position.x = gameObject.transform.position.x + cameraOffset.x;
-		position.z = gameObject.transform.position.z + cameraOffset.z;
-		mainCamera.transform.position = position;
+	Vector3 CalculateNewCameraPosition(Vector3 airplane){
+		return new Vector3(airplane.x, targetCameraAltitude, airplane.z);
+	}
+
+	Vector3 CalculateNewPosition(Vector3 position){
+		position += speed * gameObject.transform.up * Time.deltaTime;
+		position.y = heightFromSpeed + speed;
+		return position;
+	}
+
+	Quaternion CalculateNewRotation(Quaternion rotation, float yaw){
+		var yawRotation = Quaternion.AngleAxis(yaw, Vector3.up);
+		return yawRotation * rotation; 
+	}
+
+	float CalculateNewSpeed(float acceleration){
+		var newSpeed = speed + (acceleration - glideRate) * Time.deltaTime;
+		return Mathf.Clamp(newSpeed, 0.0f, targetCameraAltitude);
+	
 	}
 }
