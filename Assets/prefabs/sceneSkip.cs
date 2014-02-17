f4c13e99a2b547149b7eeab0d377a16be4cbff87
csinders <csinders@gmail.com>
2014-02-17 18:13:04 -0500

    minor minor changes

---
+++ Assets/prefabs/sceneSkip.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class sceneSkip : MonoBehaviour {
+	public string nextScene; 
+
+    void Update() {
+	    if (Input.GetKeyDown("space"))
+		    Application.LoadLevel(nextScene);
+    }
+}
