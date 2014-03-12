068c01139d4f4521e5f72318d9b3e5b96ba81b3c
Robert Dionne <robertsdionne@gmail.com>
2014-03-12 14:36:45 -0400

    Update Prototype1 scene to have layered regions. Increase the ratio of a given region based on where the player is.

---
+++ Assets/Scripts/Layers.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Layers : MonoBehaviour {
+
+  public float playerPosition;
+  public GameObject[] layers;
+  public GameObject camera;
+
+  public int layerIndex;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    playerPosition += Input.GetAxis("Horizontal") / 10;
+    if (Input.GetKeyDown("up")) {
+      layerIndex += 1;
+      for (var i = 0; i < layers.Length; ++i) {
+        layers[i].GetComponent<Region>().DoubleRegion(playerPosition);
+      }
+    }
+	  for (var i = 0; i < layers.Length; ++i) {
+      var position = layers[i].transform.position;
+      var newPosition = new Vector3(position.x, position.y, Mathf.Pow(2, (i - layerIndex) - 1));
+      layers[i].transform.position = Vector3.Lerp(position, newPosition, 0.25f);
+    }
+    var cameraPosition = camera.transform.position;
+    camera.transform.position = new Vector3(playerPosition, cameraPosition.y, cameraPosition.z);
+	}
+}
---
+++ Assets/Scripts/Region.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Region : MonoBehaviour {
+
+  public GameObject region1, region2, region3;
+  public float german, russian, wilderness;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    // Normalized means that (german + russian + wilderness) = 1
+    float normalizedGerman = german / (german + russian + wilderness);
+    float normalizedRussian = russian / (german + russian + wilderness);
+    float normalizedWilderness = wilderness / (german + russian + wilderness);
+
+    float widthGerman = 3 * normalizedGerman;
+    float widthWilderness = 3 * normalizedWilderness;
+    float widthRussian = 3 * normalizedRussian;
+
+    region2.transform.localScale = new Vector3(widthRussian, 1, 1);
+    region1.transform.localScale = new Vector3(widthWilderness, 1, 1);
+    region3.transform.localScale = new Vector3(widthGerman, 1, 1);
+
+    region2.transform.position = gameObject.transform.position + new Vector3(widthRussian / 2, 0, 0);
+    region1.transform.position = gameObject.transform.position + new Vector3(widthRussian + widthWilderness / 2, 0, 0);
+    region3.transform.position = gameObject.transform.position + new Vector3(widthRussian + widthWilderness + widthGerman / 2, 0, 0);
+	}
+
+  public void DoubleRegion(float playerPosition) {
+    var region = RegionPlayerInhabits(playerPosition);
+    if (region == 1) {
+      russian *= 2;
+    } else if (region == 2) {
+      wilderness *= 2;
+    } else {
+      german *= 2;
+    }
+  }
+
+  int RegionPlayerInhabits(float playerPosition) {
+    // Normalized means that (german + russian + wilderness) = 1
+    float normalizedGerman = german / (german + russian + wilderness);
+    float normalizedRussian = russian / (german + russian + wilderness);
+    float normalizedWilderness = wilderness / (german + russian + wilderness);
+
+    float widthGerman = 3 * normalizedGerman;
+    float widthWilderness = 3 * normalizedWilderness;
+    float widthRussian = 3 * normalizedRussian;
+
+    if (playerPosition <= widthRussian) {
+      return 2;
+    } else if (widthRussian < playerPosition && playerPosition <= widthRussian + widthWilderness) {
+      return 1;
+    } else {
+      return 3;
+    }
+  }
+}
